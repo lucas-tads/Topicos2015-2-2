07d354db9be07cb77ad9b10daffd847e5095c0e1
Lucas Teodoro Dos Santos <lucas_teodoro775@hotmail.com>
2015-09-28 20:05:02 -0300

    primeiro commit

---
+++ Flappybird/desktop/src/br/grupointegrado/ads/flappybird/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package br.grupointegrado.ads.flappybird.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import br.grupointegrado.ads.flappybird.Maingame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Maingame(), config);
+	}
+}
