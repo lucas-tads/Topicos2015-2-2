7b3a119613d0afc9709fe7aafd7f011ad0fd492c
Lucas Teodoro Dos Santos <lucas_teodoro775@hotmail.com>
2015-10-26 20:54:06 -0200

    implementacao de obstaculos

---
+++ Flappybird/core/src/br/grupointegrado/ads/flappybird/Obstaculo.java
@@ -0,0 +1,81 @@
+package br.grupointegrado.ads.flappybird;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+
+/**
+ * Created by lucas on 26/10/2015.
+ */
+public class Obstaculo {
+
+    private World mundo;
+    private OrthographicCamera camera;
+    private Body corpoCima, corpoBaixo;
+    private float posx;
+    private float posyCima, posyBaixo;
+    private float largura, altura;
+    private boolean passou;
+    private Obstaculo ultimoObstaculo; //ultimo obstaculo antes do atual
+    private float x;
+
+    public Obstaculo(World mundo, OrthographicCamera camera, Obstaculo ultimoObstaculo) {
+        this.mundo = mundo;
+        this.camera = camera;
+        this.ultimoObstaculo = ultimoObstaculo;
+
+        initposicao();
+        initCorpocima();
+        initCorpoBaixo();
+
+    }
+
+    private void initCorpoBaixo() {
+        corpoBaixo = Util.criarcorpo(mundo, BodyDef.BodyType.StaticBody, posx, posyBaixo);
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(largura / 2, altura / 2);
+
+        Util.criarforma(corpoBaixo, shape, "OBSTACULO_BAIXO");
+        shape.dispose();
+    }
+
+    private void initCorpocima() {
+        corpoCima = Util.criarcorpo(mundo, BodyDef.BodyType.StaticBody, posx, posyCima);
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(largura/ 2, altura/2);
+
+        Util.criarforma(corpoCima, shape, "OBSTACULO_CIMA");
+        shape.dispose();
+    }
+
+    private void initposicao() {
+        largura = 40 /Util.PIXEL_METRO;
+        altura = camera.viewportHeight / Util.PIXEL_METRO;
+
+        float xinicial = largura;
+        if (ultimoObstaculo != null)
+            xinicial = ultimoObstaculo.getposx();
+
+        posx= xinicial + 4;
+        //parcela é o tamanho da  tela dividido por6
+        // para encontrar a posicao de y do obstáculo
+        float parcela = (altura - Util.ALTURA_CHAO) / 6;
+
+        int multiplicador = MathUtils.random(1, 3);
+
+        posyBaixo = Util.ALTURA_CHAO + (parcela * multiplicador) - (altura / 2);
+        posyCima = posyBaixo + altura + 2f; // 2f altura entre os canos
+    }
+
+    public void remover(){
+        mundo.destroyBody(corpoCima);
+        mundo.destroyBody(corpoBaixo);
+    }
+
+    public float getposx() {
+        return this.posx;
+    }
+}
--- Flappybird/core/src/br/grupointegrado/ads/flappybird/Telajogo.java
+++ Flappybird/core/src/br/grupointegrado/ads/flappybird/Telajogo.java
@@ -12,7 +12,7 @@ import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 
 /**
- * Created by Willys on 28/09/2015.
+ * Created by Lucas on 28/09/2015.
  */
 public class Telajogo extends TelaBase {
 
@@ -34,6 +34,8 @@ public class Telajogo extends TelaBase {
         mundo = new World(new Vector2(0,-9.8f), false);
         initChao();
         initPassaro();
+
+        new Obstaculo(mundo, camera, null);
     }
 
     private void initChao() {
@@ -90,10 +92,8 @@ public class Telajogo extends TelaBase {
      * Atualiza a posiÃ§Ã£o do chÃ£o para acompanhar o pÃ¡ssaro
      */
     private void atualizarChao() {
-        float largura = camera.viewportWidth / Util.PIXEL_METRO;
-        Vector2 posicao = chao.getPosition();
-        posicao.x = largura / 2;
-        chao.setTransform(posicao, 0);
+        Vector2 posicao = passaro.getCorpo().getPosition();
+        chao.setTransform(posicao.x, 0, 0);
     }
 
     /**
@@ -138,129 +138,3 @@ public class Telajogo extends TelaBase {
         mundo.dispose();
     }
 }
-
-
-
-//
-//import com.badlogic.gdx.Gdx;
-//import com.badlogic.gdx.graphics.GL20;
-//import com.badlogic.gdx.graphics.OrthographicCamera;
-//import com.badlogic.gdx.math.Vector2;
-//import com.badlogic.gdx.physics.box2d.Body;
-//import com.badlogic.gdx.physics.box2d.BodyDef;
-//import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-//import com.badlogic.gdx.physics.box2d.Fixture;
-//import com.badlogic.gdx.physics.box2d.PolygonShape;
-//import com.badlogic.gdx.physics.box2d.World;
-//
-///**
-// * Created by lucas on 28/09/2015.
-// */
-//public class Telajogo extends TelaBase {
-//
-//    private OrthographicCamera camera; // representa a camera
-//    private World mundo; // representa a camera
-//    private Body chao; //corpo do chao
-//    private Passaro passaro;
-//
-//    private Box2DDebugRenderer debug;  //desenha o mundo na tela para ajudar o desenvolvimento
-//
-//    public Telajogo(Maingame game) {
-//        super(game);
-//    }
-//
-//    @Override
-//    public void show() {
-//        camera = new OrthographicCamera(Gdx.graphics.getWidth() / Util.ESCALA, Gdx.graphics.getHeight() / Util.ESCALA);
-//        debug = new Box2DDebugRenderer();
-//        mundo = new World(new Vector2(0, -9.8f), false);
-//
-//        initchao();
-//        initpassaro();
-//    }
-//
-//    private void initchao() {
-//        chao = Util.criarcorpo(mundo, BodyDef.BodyType.StaticBody, 0 , 0);
-//
-//
-//
-//    }
-//
-//    private void initpassaro() {
-//        passaro = new Passaro(mundo, camera, null);
-//    }
-//
-//    @Override
-//    public void render(float delta) {
-//        Gdx.gl.glClearColor(.25f, .25f, .25f, 1); //limpa a tela e pinta a cor de fundo
-//        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); //mantem o buffer de cores
-//
-//        atualizar(delta);
-//        renderizar(delta);
-//
-//        debug.render(mundo, camera.combined.cpy().scl(Util.PIXEL_METRO));
-//    }
-//
-//    /**
-//     * atulizacao e calculos dos corpos
-//     * @param delta
-//     */
-//    private void atualizar(float delta) {
-//        mundo.step(1f / 60f, 6, 2);
-//        atualizarchao();
-//    }
-//
-//    /**
-//     * atualiza a posicao do chao para acompanhar a posicao do passaro
-//     */
-//    private void atualizarchao() {
-//        float largura = camera.viewportWidth / Util.PIXEL_METRO;
-//        Vector2 posicao = new Vector2();
-//        posicao.x = largura /2;
-//        chao.setTransform(posicao, 0);
-//    }
-//
-//    /**
-//     * desenhar as imagens
-//     * @param delta
-//     */
-//    private void renderizar(float delta) {
-//
-//    }
-//
-//    @Override
-//    public void resize(int width, int height) {
-//        camera.setToOrtho(false, width / Util.ESCALA, height / Util.ESCALA);
-//        camera.update();
-//
-//        redimensionachao();
-//    }
-//
-//    /**
-//     * configura o tamanho do chao de acordo com o tamanho da tela.
-//     */
-//    private void redimensionachao() {
-//        chao.getFixtureList().clear();
-//        float largura = camera.viewportWidth / Util.PIXEL_METRO;
-//        PolygonShape shape = new PolygonShape();
-//        shape.setAsBox(largura/ 2, Util.ALTURA_CHAO / 2);
-//        Fixture forma = Util.criarforma(chao, shape, "CHAO");
-//        shape.dispose();
-//    }
-//
-//    @Override
-//    public void pause() {
-//
-//    }
-//
-//    @Override
-//    public void resume() {
-//
-//    }
-//
-//    @Override
-//    public void dispose() {
-//        mundo.dispose();
-//        debug.dispose();
-//    }
-//}
